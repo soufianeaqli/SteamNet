054d39550e8c7af5f37235531c76e0c29cc5a3fc
Aqli Soufiane <aqlisoufiane364@gmail.com>
2026-04-03 16:34:07 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173',"https://steam-net.vercel.app")],
 
     'allowed_origins_patterns' => [],
 
